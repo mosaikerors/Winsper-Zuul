3be0694f2154a72ce33a811b39ec26d963516a9b
sjyjytu <1332372004@qq.com>
2019-07-05 11:10:51 +0800

    initial

---
+++ src/main/java/com/mosaiker/winserzuul/WinserZuulApplication.java
@@ -0,0 +1,13 @@
+package com.mosaiker.winserzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinserZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinserZuulApplication.class, args);
+    }
+
+}
